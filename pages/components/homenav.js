a1e86bb05dd67a79fdf7f05a4e3dfd18fa960a0a
Nikita22r <rokhadenikita@gmail.com>
2024-02-28 13:21:41 +0530

    5n

--- pages/components/homenav.js
+++ pages/components/homenav.js
@@ -9,6 +9,9 @@ const Homenav = () => {
   return (
     <div  className="7self-stretch overflow-y-auto flex flex-col md:flex-row md:items-center items-start justify-start text-left text-base text-dwhite font-popins">
       <div className="relative bg-white w-[1550px] h-[80px]">
+      <div class="absolute inset-0 flex items-center justify-center">
+              <div class="mx-auto">idrivegen</div>
+            </div>
       <BsButton
          className="absolute top-[calc(50%_-_20px)] left-[calc(50%_+_515.54px)] cursor-pointer"
           variant="outline-primary"
@@ -24,22 +27,35 @@ const Homenav = () => {
           Kenoics login
         </BsButton>
         <BsButton
-          className="absolute w-[calc(100%_-_1410px)] top-[17.4px] right-[196.84px] left-[102.16px] cursor-pointer"
+          className="absolute w-[calc(100%_-_1450px)] top-[17.4px] right-[196.84px] left-[102.16px] cursor-pointer"
           variant="outline-primary"
           onClick={() => router.push('./certificate/cform')}
         >
-          CERTIFICATE
+          Certificate
         </BsButton>
         <BsButton
-          className="absolute w-[calc(100%_-_1410px)] top-[17.4px] right-[11.53px] left-[232.47px] cursor-pointer"
+          className="absolute w-[calc(100%_-_1450px)] top-[17.4px] right-[11.53px] left-[232.47px] cursor-pointer"
           variant="outline-primary"
           onClick={() => router.push('./licence/lform')}
         >
-         LICENSES
+          licences
         </BsButton>
+        <div dir="rtl">
+          <div class="relative h-32 w-32 ...">
+                       
+            
+          </div>
+
+          </div>
+
+<div>
+</div>
+  
+</div>
+
       </div>
 
-    </div>
+  
   );
 };
 
