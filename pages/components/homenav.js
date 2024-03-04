4163a55821e7dce82d7f890243bd0f90bf6f5d47
P Sandeep Rao <142883216+srao116@users.noreply.github.com>
2024-03-04 11:36:04 +0530

    rao

--- pages/components/homenav.js
+++ pages/components/homenav.js
@@ -10,14 +10,14 @@ const Homenav = () => {
     <div  className="7self-stretch overflow-y-auto flex flex-col md:flex-row md:items-center items-start justify-start text-left text-base text-dwhite font-popins">
       <div className="relative bg-white w-[1550px] h-[80px]">
       <BsButton
-         className="absolute top-[calc(50%_-_20px)] left-[calc(50%_+_515.54px)] cursor-pointer"
+         className="relative top-[calc(50%_-_20px)] left-[calc(47%_+_515.54px)] cursor-pointer"
           variant="outline-primary"
           onClick={() => router.push('./RTO/rtologin')}
         >
           RTO LOGIN
         </BsButton>
         <BsButton
-          className="absolute top-[calc(50%_-_20px)] left-[calc(50%_+_612.09px)] cursor-pointer"
+          className="absolute top-[calc(50%_-_20px)] left-[calc(49%_+_612.09px)] cursor-pointer"
           variant="outline-primary"
           onClick={() => router.push('./keonics/klogin')}
         >
