0daee83e70dba9a518ec8f484ef31b0666563c61
kartik9844 <96242043+kartik9844@users.noreply.github.com>
2024-04-11 15:31:22 +0530

    k42

--- pages/components/homebody.js
+++ pages/components/homebody.js
@@ -1,16 +1,14 @@
-import { useCallback } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
-
+import { Button } from 'react-bootstrap';
+import 'tailwindcss/tailwind.css';
 
 const Homebody = () => {
   return (
-    <div className="self-stretch bg-dwhite overflow-hidden flex flex-row items-center justify-center text-left text-31xl text-darkslategray-100 font-popins">
+    <div className="self-stretch bg-C1C8E4 overflow-hidden flex flex-row items-center justify-center text-left text-2xl text-darkslategray-100 font-popins">
       <div className="flex-1 overflow-hidden flex flex-row py-14 pr-[155.7999725341797px] pl-[155.8000030517578px] items-center justify-center">
         <div className="w-[400px] h-[510px] flex flex-col items-start justify-start gap-[19px] max-w-[2018px]">
           <div className="w-[400px] flex flex-row items-start justify-start">
             <h1 className="m-0 relative text-inherit tracking-[-2px] leading-[35px] font-normal font-popins inline-block max-w-[800px]">
-              <p>{`Blockchain based driving license and course completion certificate generation and validation system`}</p>
+              <p className="hover:text-5AB9EA focus:text-8860D0 active:text-primary">{`Blockchain based driving license and course completion certificate generation and validation system`}</p>
             </h1>
           </div>
           <div className="w-[420px] h-[510px]  flex-row items-start justify-start text-xs text-gray">
@@ -18,7 +16,6 @@ const Homebody = () => {
               <p className="m-0 text-lg">{`The blockchain-based solution on the Polygon network revolutionizes online certification and licensing processes. With distinct portals for the RTO and KIONCS, users can seamlessly generate and validate licenses and certificates. Leveraging Polygon's blockchain ensures a secure environment, minimizing fraud risks and enhancing trust. This system addresses the pressing need for streamlined and secure certification procedures. Its user-friendly interface and blockchain technology pave the way for a future of digital governance focused on transparency and efficiency.`}</p>
             </h2>
           </div>
-          
         </div>
       </div>
       <img
@@ -31,5 +28,3 @@ const Homebody = () => {
 };
 
 export default Homebody;
-
-
--- pages/components/homenav.js
+++ pages/components/homenav.js
@@ -2,36 +2,36 @@ import { useCallback } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button as BsButton } from "react-bootstrap";
 import { useRouter } from 'next/navigation'
-
+import 'tailwindcss/tailwind.css'
 
 const Homenav = () => {
     const router = useRouter()
   return (
-    <div  className="7self-stretch overflow-y-auto flex flex-col md:flex-row md:items-center items-start justify-start text-left text-base text-dwhite font-popins">
+    <div className="7self-stretch overflow-y-auto flex flex-col md:flex-row md:items-center items-start justify-start text-left text-base text-dwhite font-popins">
       <div className="relative bg-white w-[1550px] h-[80px]">
       <BsButton
-         className="relative top-[calc(50%_-_20px)] left-[calc(47%_+_515.54px)] cursor-pointer"
+         className="relative top-[calc(50%_-_20px)] left-[calc(47%_+_515.54px)] cursor-pointer hover:bg-[#5680E9] focus:bg-[#84CEEB] active:bg-[#5AB9EA] text-black"
           variant="outline-primary"
           onClick={() => router.push('./RTO/rtologin')}
         >
           RTO LOGIN
         </BsButton>
         <BsButton
-          className="absolute top-[calc(50%_-_20px)] left-[calc(49%_+_612.09px)] cursor-pointer"
+          className="absolute top-[calc(50%_-_20px)] left-[calc(49%_+_612.09px)] cursor-pointer hover:bg-[#5680E9] focus:bg-[#84CEEB] active:bg-[#5AB9EA] text-black"
           variant="outline-primary"
           onClick={() => router.push('./keonics/klogin')}
         >
           KEONICS LOGIN
         </BsButton>
         <BsButton
-          className="absolute w-[calc(100%_-_1410px)] top-[17.4px] right-[196.84px] left-[102.16px] cursor-pointer"
+          className="absolute w-[calc(100%_-_1410px)] top-[17.4px] right-[196.84px] left-[102.16px] cursor-pointer hover:bg-[#5680E9] focus:bg-[#84CEEB] active:bg-[#5AB9EA] text-black"
           variant="outline-primary"
           onClick={() => router.push('./certificate/cform')}
         >
           CERTIFICATE
         </BsButton>
         <BsButton
-          className="absolute w-[calc(100%_-_1410px)] top-[17.4px] right-[11.53px] left-[232.47px] cursor-pointer"
+          className="absolute w-[calc(100%_-_1410px)] top-[17.4px] right-[11.53px] left-[232.47px] cursor-pointer hover:bg-[#5680E9] focus:bg-[#84CEEB] active:bg-[#5AB9EA] text-black"
           variant="outline-primary"
           onClick={() => router.push('./licence/lform')}
         >
